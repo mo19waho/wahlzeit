0ef4adcb62dc92443bd48945438a6f4a3df15ee2
mo19waho <44146153+mo19waho@users.noreply.github.com>
2018-11-22 12:48:19 +0100

    Create AbstractCoordinate.java

---
+++ src/main/java/org/wahlzeit/model/AbstractCoordinate.java
@@ -0,0 +1,35 @@
+/*
+ * Interface AbstractCoordinate
+ *
+ * Version 1.0
+ *
+ * 11-22-2018
+ *
+ * Copyright (c) 2018 Lukas Batz
+ *
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.wahlzeit.model;
+
+/**
+ * AbstractCoordinate, abstract superclass, which is base of different implementations representing coordinates.
+ * Inherits from Coordinate interface.
+ */
+public abstract class AbstractCoordinate implements Coordinate
+{
+    // Coordinate Interface Methods are forced to be implemented by 
+}
